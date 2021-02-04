970e0d4a271e74b7f635e54dcafab409b8a7debf
LeeSeokWon <tjrdnjs5@gmail.com>
2021-02-05 00:46:20 +0900

    깃 확인용

--- app/src/main/java/com/dpplatform/oceancampus/MainActivity.kt
+++ app/src/main/java/com/dpplatform/oceancampus/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)//깃허브
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
     }
 }
