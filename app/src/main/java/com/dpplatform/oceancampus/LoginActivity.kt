b4d7e0b6489e47d4b49e4aac17579279edc71b0f
seokwon <tjrdnjs5@gmail.com>
2021-02-09 00:40:29 +0900

    구글 로그인 진행중

--- app/src/main/java/com/dpplatform/oceancampus/LoginActivity.kt
+++ app/src/main/java/com/dpplatform/oceancampus/LoginActivity.kt
@@ -4,25 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
+
         button_signIn.setOnClickListener {
             signinAndSignup()
         }
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("542623062976-b6n4caegjps5kurpfgnh2clmdg0mof62.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
     }
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE )
+    }
+
+
     fun signinAndSignup() {
         auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())?.addOnCompleteListener {
             task ->
                 if (task.isSuccessful){
-                    moveMainPage(task.result!!.user)
+                    moveMainPage(task.result?.user)
                 }else if (task.exception?.message.isNullOrEmpty()){
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
                 }else{
@@ -36,7 +54,7 @@ class LoginActivity : AppCompatActivity() {
         auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())?.addOnCompleteListener {
                 task ->
             if (task.isSuccessful){
-                moveMainPage(task.result!!.user)
+                moveMainPage(task.result?.user)
             }else{
                 Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
                      }
--- app/src/main/java/com/dpplatform/oceancampus/MainActivity.kt
+++ app/src/main/java/com/dpplatform/oceancampus/MainActivity.kt
@@ -2,10 +2,20 @@ package com.dpplatform.oceancampus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inspector.PropertyMapper
+import android.widget.EditText
+import androidx.annotation.VisibleForTesting
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) //ㅎㅇㅎㅇ
+        setContentView(R.layout.activity_main)
+
+
+
+
+        }
+
     }
-}
