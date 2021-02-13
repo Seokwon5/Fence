6af19ba29514869bd32b150d0b251e3134dee47b
LeeSeokWon <tjrdnjs5@gmail.com>
2021-02-13 16:42:46 +0900

    ImageUpload.

--- app/src/main/java/com/dpplatform/oceancampus/AddPhotoActivity.kt
+++ app/src/main/java/com/dpplatform/oceancampus/AddPhotoActivity.kt
@@ -1,11 +1,65 @@
 package com.dpplatform.oceancampus
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.internal.Storage
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPhotoActivity : AppCompatActivity() {
+
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var storage : FirebaseStorage? = null
+    var photoUri : Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
+        //Initate storage
+        storage = FirebaseStorage.getInstance()
+
+        //Open the Album
+        var PhotoPickerIntent = Intent(Intent.ACTION_PICK)
+        PhotoPickerIntent.type = "image/*"
+        startActivityForResult(PhotoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+
+        //add image upload event
+        addPhoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_FROM_ALBUM) {
+            if (requestCode == Activity.RESULT_OK) {
+                photoUri = data?.data
+                addPhoto_image.setImageURI(photoUri)
+
+            } else {
+                finish()
+            }
+        }
+    }
+
+    fun contentUpload() {
+
+        var timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timeStamp + "_.png"
+
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        //FileUpload
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            Toast.makeText(this, getString(R.string.upload_success),Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
--- app/src/main/java/com/dpplatform/oceancampus/MainActivity.kt
+++ app/src/main/java/com/dpplatform/oceancampus/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.dpplatform.oceancampus
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import navigation.AlarmFragment
 import navigation.DetailViewFragment
 import navigation.GridFragment
 import navigation.UserFragment
+import java.util.jar.Manifest
 
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -29,6 +34,9 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo-> {
+                if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
 
                 return true
             }
@@ -54,6 +62,7 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
 
         }
 
