0104106664fb82751d8ff0f02beb70c32b4a9bb1
LeeSeokWon <tjrdnjs5@gmail.com>
2021-02-15 00:45:41 +0900

    ImageUpload.

--- app/src/main/java/com/dpplatform/oceancampus/MainActivity.kt
+++ app/src/main/java/com/dpplatform/oceancampus/MainActivity.kt
@@ -16,47 +16,8 @@ import navigation.UserFragment
 import java.util.jar.Manifest
 
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
-    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-        when (p0.itemId) {
-            R.id.action_home -> {
-                var detailViewFragment = DetailViewFragment()
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_content, detailViewFragment)
-                    .commit()
-                return true
-            }
-            R.id.action_search -> {
-                var GridFragment = GridFragment()
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_content, GridFragment)
-                    .commit()
-                return true
-            }
-            R.id.action_add_photo-> {
-                if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                    startActivity(Intent(this,AddPhotoActivity::class.java))
-                }
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
 
-                return true
-            }
-            R.id.action_favorite_alarm -> {
-                var alarmFragment = AlarmFragment()
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_content, alarmFragment)
-                    .commit()
-                return true
-            }
-            R.id.action_account -> {
-                var userFragment = UserFragment()
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_content, userFragment)
-                    .commit()
-                return true
-            }
-        }
-        return false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,6 +27,38 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
+
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
 
 
 }
--- app/src/main/java/navigation/DetailViewFragment.kt
+++ app/src/main/java/navigation/DetailViewFragment.kt
@@ -7,13 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.dpplatform.oceancampus.R
 
-class DetailViewFragment:Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        var view =LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
+class DetailViewFragment:Fragment() { override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+    var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail,container,false)
         return view
     }
 }
