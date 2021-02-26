77e898707d5a5514ecca2fae73c20474cfb7fe83
seokwon <tjrdnjs5@gmail.com>
2021-02-26 18:06:40 +0900

    DetailViewFragment.kt에 말풍선 클릭시 intent를 통해 context,contentUid 넘어감.

--- app/src/main/java/com/dpplatform/oceancampus/navigation/CommentActivity.kt
+++ app/src/main/java/com/dpplatform/oceancampus/navigation/CommentActivity.kt
@@ -2,12 +2,22 @@ package com.dpplatform.oceancampus.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.request.RequestOptions
 import com.dpplatform.oceancampus.R
 import com.dpplatform.oceancampus.navigation.model.ContentDTO
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_comment.*
+import kotlinx.android.synthetic.main.item_comment.view.*
+import java.sql.Timestamp
+import java.util.zip.Inflater
 
 class CommentActivity : AppCompatActivity() {
     var contentUid : String? = null
@@ -15,6 +25,11 @@ class CommentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comment)
         contentUid = intent.getStringExtra("contentUid")
+
+        comment_recyclerview.adapter = CommentRecyclerviewAdapter()
+
+
+
 
         comment_btn_send?.setOnClickListener {
             var comment = ContentDTO.Comment()
@@ -26,11 +41,49 @@ class CommentActivity : AppCompatActivity() {
             FirebaseFirestore.getInstance().collection("images").document(contentUid!!).collection("comments").document().set(comment)
 
             comment_edit_message.setText("")
+        }
+    }
+    inner class CommentRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+        var comments : ArrayList<ContentDTO.Comment> = arrayListOf()
+        init {
+            FirebaseFirestore.getInstance().collection("images").document(contentUid!!)
+                    .collection("comments").orderBy("timestamp")
+                    .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                        comments.clear()
+                        if (querySnapshot == null)return@addSnapshotListener
 
-            
+                        for (snapshot in querySnapshot.documents!!){
+                            comments.add(snapshot.toObject(ContentDTO.Comment::class.java)!!)
+                        }
+                        notifyDataSetChanged()
+                    }
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
+             return CustomViewHolder(view)
+        }
+        private inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view)
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var view = holder.itemView
+            view.commentviewitem_textview_comment.text = comments[position].comment
+            view.commentviewitem_textview_profile.text = comments[position].userId
+
+            FirebaseFirestore.getInstance().collection("profileImages")
+                    .document(comments[position].uid!!)
+                    .get().addOnCompleteListener { task ->
+                        if (task.isSuccessful){
+                            var url = task.result!!["images"]
+                            Glide.with(holder.itemView.context).load(url).apply(RequestOptions().circleCrop()).into(view.commentviewitem_imageview_profile)
+                        }
+                    }
+
         }
 
+        override fun getItemCount(): Int {
+            return comments.size
+        }
 
     }
 }
